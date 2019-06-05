92e8b58a040c9223750014ffa2ffa7cd5696d100
未来可期 <1163220839@qq.com>
2019-06-05 23:08:10 +0800

    寻找art-template模板中数据循环嵌套的解决方法

--- gulpfile-dev.js
+++ gulpfile-dev.js
@@ -1,27 +1,3 @@
-// const gulp = require("gulp")
-// const webserver = require("gulp-webserver")
-
-// //拷贝  index.html 到dev根目录下
-// //任务的回调一定要有返回值，返回值全部都是 异步操作
-// //如果不返回值，需要调用callback
-
-
-// gulp.task('copyhtml',()=>{
-//     gulp.src('./index.html')
-//     .pipe(gulp.dest('./dev/'))
-// })
-
-
-// gulp.task("webserver",()=>{
-//     return gulp.src('./dev')
-//     .pipe(webserver({
-//         port:8000,
-//         livereload:true
-//     }))
-// })
-
-// gulp.task('default',gulp.series('copyhtml'))
-
 
 //解构赋值
 const { src, dest, series, parallel, watch } = require('gulp')  //series串行   parallel并行
@@ -52,6 +28,11 @@ function copylibs() {
 function copymock() {
     return src('./src/mock/**/*')
         .pipe(dest('./dev/mock'))
+}
+
+function copyrouter() {
+    return src('./src/router/**/*')
+        .pipe(dest('./dev/router'))
 }
 
 function copyimgs() {
@@ -147,13 +128,15 @@ function gulpWebserver() {
 
 function watcher() {
     watch('./src/styles/**/*', series(packCSS))
-    watch(['./src/**/*', '!./src/libs/**/*', '!./src/styles/**/*','!./src/icons/**/*','!./src/imgs/**/*'], series(packjs)) //!表示除去
+    watch(['./src/**/*', '!./src/libs/**/*', '!./src/styles/**/*','!./src/icons/**/*','!./src/router/**/*','!./src/imgs/**/*'], series(packjs)) //!表示除去
     watch('./src/libs/**/*', series(clear('./dev/libs'),copylibs))
     watch('./src/imgs/**/*', series(clear('./dev/imgs'), copyimgs))
     watch('./src/icons/**/*', series(clear('./dev/icons'), copyicons))
     watch('./*.html', series(clear('./dev/*.html'), copyhtml))
+    watch('./src/router/**/*', series(clear('./dev/router'), copyrouter))
+
     
 }
 //commonjs规范的default可以直接运行
 //exports.  将任务暴露出去，外部就可以访问
-exports.default = series(parallel(packjs, packCSS, copylibs, copyimgs, copyicons), copyhtml, gulpWebserver, watcher)
+exports.default = series(parallel(packjs, packCSS, copylibs, copyimgs, copyicons,copyrouter), copyhtml, gulpWebserver, watcher)
--- src/controllers/list.js
+++ src/controllers/list.js
@@ -1,4 +1,7 @@
 const goodslistTpl = require('../views/goodslist.html')
+const goodslist0 = require('../views/goods/goods0.html')
+const goodslist1 = require('../views/goods/goods1.html')
+const BScroll = require('better-scroll').default
 
 module.exports = {
     goodslist() {
@@ -8,10 +11,30 @@ module.exports = {
             success(result) {
                 const resultstr = JSON.parse(result);
                 const data = resultstr.data;
-                console.log(data);
+                //本次需要用到的数据，但是出现了循环嵌套
+                // console.log(resultstr.data.article_list);
+                // const list0data = resultstr.data.article_list[0].ads_info;
+                // console.log(list1data);
+                // const list1data = resultstr.data.article_list[1].ads_info;
 
-                const renderedGoodsTpl = template.render(goodslistTpl, { data })   // 将模板源代码编译成函数并立刻执行 第一个参数 来源 第二个参数{里面传数据} 
+
+                const renderedGoodsTpl = template.render(goodslistTpl, {data })   // 将模板源代码编译成函数并立刻执行 第一个参数 来源 第二个参数{里面传数据} 
                 document.querySelector('#goodslist').innerHTML = renderedGoodsTpl;
+
+                // const renderedGoodsTpl0 = template.render(goodslist0, { list0data })   // 将模板源代码编译成函数并立刻执行 第一个参数 来源 第二个参数{里面传数据} 
+                // document.querySelector('#goodslist').innerHTML = renderedGoodsTpl0;
+                // const renderedGoodsTpl1 = template.render(goodslist1, { list1data })   // 将模板源代码编译成函数并立刻执行 第一个参数 来源 第二个参数{里面传数据} 
+                // document.querySelector('#goodslist').innerHTML = renderedGoodsTpl1;
+                
+                
+                //添加滚动
+                
+                // let bScroll = new BScroll('.list2', {
+                //     probeType: 1,
+                //     click: true,
+                //     eventPassthrough: "vertical"
+
+                //   })
 
 
             }
